ff29a38b92d347bb707e919d9ac8d34b4ac969fb
Vikas Shetty <vikasrs4u@gmail.com>
2018-05-28 16:37:09 -0700

    Added age validation for registeration.
    If user is above 13 years old then only he/she will be allowed to register to the system.

--- app/src/main/java/com/example/yuxuanli/pinmoon/Login/RegisterAge.java
+++ app/src/main/java/com/example/yuxuanli/pinmoon/Login/RegisterAge.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.example.yuxuanli.pinmoon.R;
 import com.example.yuxuanli.pinmoon.Utils.User;
+
+import java.util.Calendar;
 
 public class RegisterAge extends AppCompatActivity {
 
@@ -29,6 +32,7 @@ public class RegisterAge extends AppCompatActivity {
 
         ageSelectionPicker = (DatePicker) findViewById(R.id.ageSelectionPicker);
 
+
         ageContinueButton = (Button) findViewById(R.id.ageContinueButton);
 
         ageContinueButton.setOnClickListener(new View.OnClickListener() {
@@ -44,9 +48,38 @@ public class RegisterAge extends AppCompatActivity {
 
     public void openHobbiesEntryPage()
     {
-        Intent intent = new Intent(this, RegisterHobby.class);
-        intent.putExtra("password", password);
-        intent.putExtra("classUser", user);
-        startActivity(intent);
+        int age = getAge(ageSelectionPicker.getYear(),ageSelectionPicker.getMonth(),ageSelectionPicker.getDayOfMonth());
+
+        // if user is above 13 years old then only he/she will be allowed to register to the system.
+        if (age > 13)
+        {
+            Intent intent = new Intent(this, RegisterHobby.class);
+            intent.putExtra("password", password);
+            intent.putExtra("classUser", user);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Age of the user should be greater than 13 !!!",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    // method to get the current age of the user.
+    private int getAge(int year, int month, int day)
+    {
+        Calendar dateOfBirth = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
+
+        dateOfBirth.set(year, month, day);
+
+        int age = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR);
+
+        if (today.get(Calendar.DAY_OF_YEAR) < dateOfBirth.get(Calendar.DAY_OF_YEAR))
+        {
+            age--;
+        }
+
+        return age;
     }
 }
