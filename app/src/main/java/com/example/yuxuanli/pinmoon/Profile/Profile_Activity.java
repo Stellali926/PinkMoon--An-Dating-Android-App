23407b65b1e5bc0b887b76f85eb8c9669dde377d
yuxuanli <lyx.stella@hotmail.com>
2018-06-04 18:58:56 -0700

    Update profile page circle photo

--- app/src/main/java/com/example/yuxuanli/pinmoon/Profile/Profile_Activity.java
+++ app/src/main/java/com/example/yuxuanli/pinmoon/Profile/Profile_Activity.java
@@ -3,34 +3,56 @@ package com.example.yuxuanli.pinmoon.Profile;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.example.yuxuanli.pinmoon.Login.Login;
 import com.example.yuxuanli.pinmoon.R;
 import com.example.yuxuanli.pinmoon.Utils.TopNavigationViewHelper;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.Map;
 
 public class Profile_Activity extends AppCompatActivity {
     private static final String TAG = "Profile_Activity";
     private static final int ACTIVITY_NUM = 0;
 
     private Context mContext = Profile_Activity.this;
+    private ImageView imagePerson;
+
+    private String userId;
 
     //firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference mPhotoDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        setupFirebaseAuth();
         setupTopNavigationView();
+        userId = mAuth.getInstance().getCurrentUser().getUid();
+        imagePerson = findViewById(R.id.circle_profile_image);
+
+        findUser();
 
         ImageButton edit_btn = (ImageButton) findViewById(R.id.edit_profile);
         edit_btn.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +73,99 @@ public class Profile_Activity extends AppCompatActivity {
         });
     }
 
+    public void findUser() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference maleDb = FirebaseDatabase.getInstance().getReference().child("male");
+        maleDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                if (dataSnapshot.getKey().equals(userId)) {
+                    Log.d(TAG, "onChildAdded: the sex is male" );
+                    mPhotoDB = FirebaseDatabase.getInstance().getReference().child("male").child(userId);
+                    getUserPhoto();
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        DatabaseReference femaleDb = FirebaseDatabase.getInstance().getReference().child("female");
+        femaleDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                if (dataSnapshot.getKey().equals(user.getUid())) {
+                    Log.d(TAG, "onChildAdded: the sex is female" );
+                    mPhotoDB = FirebaseDatabase.getInstance().getReference().child("female").child(userId);
+                    getUserPhoto();
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+
+    private void getUserPhoto(){
+        mPhotoDB.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if (map.get("profileImageUrl") != null) {
+                        String profileImageUri = map.get("profileImageUrl").toString();
+                        Log.d(TAG, "onDataChange: the profileImageUri is" + profileImageUri);
+
+                        switch (profileImageUri) {
+                            case "defaultFemale":
+                                Glide.with(getApplication()).load(R.drawable.default_woman).into(imagePerson);
+                                break;
+                            case "defaultMale":
+                                Glide.with(getApplication()).load(R.drawable.default_man).into(imagePerson);
+                                break;
+                            default:
+                                Glide.with(getApplication()).load(profileImageUri).into(imagePerson);
+                                break;
+                        }
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
 
     private void setupTopNavigationView() {
         Log.d(TAG, "setupTopNavigationView: setting up TopNavigationView");
@@ -60,6 +175,55 @@ public class Profile_Activity extends AppCompatActivity {
         Menu menu = tvEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+
+    //----------------------------------------Firebase----------------------------------------
+
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth(){
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // user is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in:" + user.getUid());
+                } else {
+                    //user is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+
+                    Log.d(TAG, "onAuthStateChanged: navigating back to login screen.");
+                    Intent intent = new Intent(Profile_Activity.this, Login.class);
+
+                    //clear the activity stack， in case when sign out, the back button will bring the user back to the previous activity
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 }
